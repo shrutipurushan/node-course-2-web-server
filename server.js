9288f977c2acb5a4efc9a5976a975e5bcc703434
shrutip@aissel.com <shrutip@aissel.com>
2017-12-31 17:55:09 +0530

    'SETUP_START_SCRIPT_AND_HEROKU_PORT'

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -28,9 +30,9 @@ app.use((req,res,next) => {
     next();
 });
 
-app.use((req,res,next) => {
-    res.render('maintanence.hbs')
-});
+// app.use((req,res,next) => {
+//     res.render('maintanence.hbs')
+// });
 
 // app.get('/',(req,res) => {
 //     //res.send('<h1>Hello Express!</h1>');
@@ -66,6 +68,6 @@ app.get('/bad', (req,res) => {
 
 //app.listen(3000);
 
-app.listen(3000 , () => {
-    console.log('Server is up on port 3000');
+app.listen(port , () => {
+    console.log(`Server is up on port ${port}`);
 })
